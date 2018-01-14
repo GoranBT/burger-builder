83bb9654e50f54249f53e945912ad59a9d46d1ad
GoranBT <goran.angelovski.bt@gmail.com>
2018-01-14 19:35:38 +0000

    Using redux to manage the checkout component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,7 @@ class BurgerBuilder extends Component {
   state = {
     // ingredients: null,
     // totalPrice: 4,
-    purchasable: false,
+    // purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -49,7 +49,8 @@ class BurgerBuilder extends Component {
     .reduce( ( sum, el ) => {
       return sum + el;
     }, 0 );
-    this.setState( { purchasable: sum > 0 } );
+    // this.setState( { purchasable: sum > 0 } );
+    return sum > 0;
   }
 
 
@@ -96,16 +97,16 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-
-    const queryParams = [];
-    for (let i in this.state.ingredients) {
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-    }
-    queryParams.push('price=' + this.state.totalPrice);
-    const queryString = queryParams.join('&');
+    // this is managed using redux
+    // const queryParams = [];
+    // for (let i in this.state.ingredients) {
+    //   queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    // }
+    // queryParams.push('price=' + this.state.totalPrice);
+    // const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
-      search: '?' + queryString
+      // search: '?' + queryString
     });
   }
 
@@ -127,7 +128,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ing)}
             ordered={this.purchaseHandler}
             price={this.props.price} />
           </Aux>
